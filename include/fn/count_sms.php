e462251b988de3323f3b446495ef4650abbeec9b
jrealm <jrealm@gmail.com>
2021-12-08 15:50:35 +0800

    improve Func::count_sms()

--- include/fn/count_sms.php
+++ include/fn/count_sms.php
@@ -1,8 +1,14 @@
 <?php //>
 
-return function ($ip, $seconds) {
+return function ($conditions, $seconds) {
     $table = table('SmsLog');
     $timestamp = date(cfg('system.timestamp'), time() - $seconds);
 
-    return $table->model()->count(['ip' => $ip, $table->create_time->GreaterThan($timestamp)]);
+    if (!is_array($conditions)) {
+        $conditions = ['ip' => $conditions];
+    }
+
+    $conditions[] = $table->create_time->GreaterThan($timestamp);
+
+    return $table->model()->count($conditions);
 };
--- include/fn/send_sms.php
+++ include/fn/send_sms.php
@@ -1,5 +1,7 @@
 <?php //>
 
+use Twilio\Rest\Client;
+
 return new class() {
 
     public function __invoke($args) {
@@ -8,13 +10,15 @@ return new class() {
 
     private function log($args, $response) {
         model('SmsLog')->insert([
-            'receiver' => $args['mobile'],
+            'receiver' => $args['receiver'],
             'content' => $args['text'],
             'response' => $response,
         ]);
     }
 
     private function mitake($args) {
+        $args['receiver'] = $args['mobile'];
+
         if ($args['prefix']) {
             $args['mobile'] = $args['prefix'] . ltrim($args['mobile'], '0');
         }
@@ -32,6 +36,18 @@ return new class() {
         }
 
         return false;
+    }
+
+    private function twilio($args) {
+        if ($args['prefix']) {
+            $args['mobile'] = $args['prefix'] . ltrim($args['mobile'], '0');
+        }
+logger('error')->info(json_encode($args, JSON_UNESCAPED_UNICODE));
+        $client = new Client($args['key'], $args['screct']);
+
+        $message = $client->messages->create($args['mobile'], ['body' => $args['text']]);
+
+        $this->log($args, json_encode($message, JSON_UNESCAPED_UNICODE));
     }
 
 };
