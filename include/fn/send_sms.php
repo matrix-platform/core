c0ebf66d03833cb0c7dd6398a4f044de99905cb7
jrealm <jrealm@gmail.com>
2021-12-08 15:52:56 +0800

    misc

--- include/fn/send_sms.php
+++ include/fn/send_sms.php
@@ -1,7 +1,5 @@
 <?php //>
 
-use Twilio\Rest\Client;
-
 return new class() {
 
     public function __invoke($args) {
@@ -36,18 +34,6 @@ return new class() {
         }
 
         return false;
-    }
-
-    private function twilio($args) {
-        if ($args['prefix']) {
-            $args['mobile'] = $args['prefix'] . ltrim($args['mobile'], '0');
-        }
-logger('error')->info(json_encode($args, JSON_UNESCAPED_UNICODE));
-        $client = new Client($args['key'], $args['screct']);
-
-        $message = $client->messages->create($args['mobile'], ['body' => $args['text']]);
-
-        $this->log($args, json_encode($message, JSON_UNESCAPED_UNICODE));
     }
 
 };
