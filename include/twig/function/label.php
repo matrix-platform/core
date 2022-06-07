0ad89813023536d0e419773952e1a69a3e926262
jrealm <jrealm@gmail.com>
2022-06-08 01:42:01 +0800

    misc

--- include/twig/function/label.php
+++ include/twig/function/label.php
@@ -1,7 +1,11 @@
 <?php //>
 
-return new Twig\TwigFunction('label', function ($token, $default = null) {
+return new Twig\TwigFunction('label', function ($token, $default = null, $data = null) {
     $text = i18n($token, $default);
+
+    if ($data) {
+        $text = render($text, $data);
+    }
 
     return new Twig\Markup("<span data-edit=\"{$token}\">{$text}</span>", 'UTF-8');
 });
