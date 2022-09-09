efbb0bf7f69045767d088bf7698347e7526c5cb3
jrealm <jrealm@gmail.com>
2022-09-09 13:35:10 +0800

    misc

--- view/native/backend/vue-component.php
+++ view/native/backend/vue-component.php
@@ -5,11 +5,11 @@ use matrix\utility\Func;
 $data = @$result['data'] ?: $form;
 $table = $controller->table();
 
-$list = $table->model()->parents($data);
+$list = $table ? $table->model()->parents($data) : [];
 $list[] = $data;
 $titles = array_filter(array_column($list, '.title'), 'is_string');
 
 $result['subtitle'] = array_pop($titles);
 $result['breadcrumbs'] = Func::breadcrumbs($controller->menus(), $controller->node(), $list);
 
-lookup($controller->customView())->render($controller, $form, $result);
+lookup('backend/vue-component.twig')->render($controller, $form, $result);
