e93fe72b6a2a94b65c0d14457844cc7ab3051d05
jrealm <jrealm@gmail.com>
2022-12-11 02:30:00 +0800

    misc

--- class/matrix/cli/Mutex.php
+++ class/matrix/cli/Mutex.php
@@ -5,7 +5,7 @@ namespace matrix\cli;
 trait Mutex {
 
     public function execute() {
-        $name = str_replace('/', '.', $this->name());
+        $name = str_replace('/', '.', $this->mutex() ?: $this->name());
         $file = fopen(create_folder(APP_DATA) . $name, 'w');
 
         if (flock($file, LOCK_EX | LOCK_NB)) {
