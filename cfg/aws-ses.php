7331e40e97e742cfeb8092dd5c419b8d2915c999
jrealm <jrealm@gmail.com>
2023-03-31 10:55:54 +0800

    AWS Simple Email Service

---
+++ cfg/aws-ses.php
@@ -0,0 +1,23 @@
+<?php //>
+
+return [
+
+    'sender' => null,
+
+    'from' => null,
+
+    'username' => null,
+
+    'password' => null,
+
+    'host' => null,
+
+    'port' => 465,
+
+    'secure' => 'ssl',
+
+    'queue' => null,
+
+    'sandbox' => null,
+
+];
