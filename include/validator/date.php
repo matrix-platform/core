73116694127b14e155a570888cd4fd813e1c9d59
jrealm <jrealm@gmail.com>
2023-05-25 07:01:45 +0800

    validator

--- include/validator/date.php
+++ include/validator/date.php
@@ -1,3 +1,12 @@
 <?php //>
 
-return load_resource('include/validator/timestamp.php');
+return function ($value, $options) {
+    $pattern = @$options['pattern'] ?: cfg('system.date');
+    $datetime = DateTime::createFromFormat($pattern, $value);
+
+    if ($datetime && $datetime->format($pattern) === $value) {
+        return true;
+    }
+
+    return false;
+};
--- include/validator/timestamp.php
+++ include/validator/timestamp.php
@@ -1,7 +1,7 @@
 <?php //>
 
 return function ($value, $options) {
-    $pattern = $options['pattern'];
+    $pattern = @$options['pattern'] ?: cfg('system.timestamp');
     $datetime = DateTime::createFromFormat($pattern, $value);
 
     if ($datetime && $datetime->format($pattern) === $value) {
